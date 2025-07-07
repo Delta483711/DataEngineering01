9df90ecd433788e0aa831909ebf09f80bf0ad3b6
Michael <59081538+Delta483711@users.noreply.github.com>
2025-07-07 14:41:11 +0100

    updated script

--- Extract_Script.py
+++ Extract_Script.py
@@ -1,6 +1,32 @@
+
+
 import requests
 import pandas as pd
 
+BASE_URL = 'https://api.covid19api.com'
+
+def get_covid19_data(country=None):
+    if country:
+        endpoint = f"{BASE_URL}/dayone/country/{country}"
+    else:
+        endpoint = f"{BASE_URL}/summary"
+        
+    response = requests.get(endpoint)
+    
+    if response.status_code == 200:
+        return response.json()
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code} - {response.text}")
+
+# Fetch global COVID-19 summary
+global_data = get_covid19_data()
+print("Global Summary:")
+print(global_data['Global'])
+
+# Fetch country-specific COVID-19 data
+country_data = get_covid19_data(country="united-kingdom")
+print("\nUK Day One Data:")
+print(pd.DataFrame(country_data).head())
 
 
 
@@ -8,26 +34,3 @@ import pandas as pd
 ##https://www.omi.me/blogs/api-guides/how-to-fetch-covid-19-data-using-covid-19-api-in-python?srsltid=AfmBOoqV0tCWposskBXLRBZjlL6n3DwPz6Xhs1VWfeElHwXnqslh4dza
 
 ##https://api.covidtracking.com
-
-BASE_URL = 'https://api.covidtracking.com'
-
-def get_covid19_data(country=None):
-    endpoint = f"{BASE_URL}summary"
-    if country:
-        endpoint = f"{BASE_URL}dayone/country/{country}"
-        
-    response = requests.get(endpoint)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return data
-    else:
-        raise Exception(f"Failed to fetch data: {response.status_code}")
-
-# Fetch global COVID-19 summary
-global_data = get_covid19_data()
-print(global_data)
-
-# Fetch country-specific COVID-19 data
-country_data = get_covid19_data(country="us")
-print(country_data)
