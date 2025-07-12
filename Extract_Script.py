ee1932137f359eff3f1e180e27ce67fe3e45dfa2
Michael <59081538+Delta483711@users.noreply.github.com>
2025-07-12 10:45:19 +0100

    Added connection to database

--- Extract_Script.py
+++ Extract_Script.py
@@ -1,10 +1,10 @@
 import requests 
 import pandas as pd 
 import datetime
+import psycopg2
 
 StartDate = datetime.datetime(2025, 1, 1)
 StopDate = datetime.datetime(2025, 7, 1)
-
 
 #Create a function to store the API Call 
 def get_uk_historical():
@@ -22,24 +22,46 @@ def get_uk_historical():
         deaths_df = pd.DataFrame(timeline['deaths'].items(), columns=['date', 'deaths'])
         recovered_df = pd.DataFrame(timeline['recovered'].items(), columns=['date', 'recovered'])
         df = cases_df.merge(deaths_df, on='date').merge(recovered_df, on='date')
-    
+
         return df
     else:
         ##Error message
         print('Failed')
         raise Exception(f"Failed to fetch data: {response.status_code}")
-    
 
 resultlist = get_uk_historical()
 
 print(resultlist)
 
+##### Connect to Neon or any other database ######
 
-##### Connect to Neon 
+username = 'neondb_owner'
+password = 'npg_0aLTWcGqE1AJ'
+host = 'ep-lively-term-abff86z3-pooler.eu-west-2.aws.neon.tech'
+port = '5432'
+database = 'neondb'
+sslmode = 'require'  # Neon requires SSL
 
 
+## Test connectiion 
 
-        
+try:
+    connection = psycopg2.connect(
+        host=host,
+        database=database,
+        user=username,
+        password=password,
+        port=port,
+        sslmode=sslmode
+    )
+    print(" Successfully connected to Neon PostgreSQL!")
+    connection.close()
+
+except Exception as e:
+    print(" Connection failed:", e)
+
+
+##### pass the data into the Neno #####
 
 
 
