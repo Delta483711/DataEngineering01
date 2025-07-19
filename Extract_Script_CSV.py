1c17b0203ff8fc34033e5bc3bf92d8f8bd13a149
Michael <59081538+Delta483711@users.noreply.github.com>
2025-07-19 10:07:19 +0100

    Update Connection Function to allow for code reusability
    Remove the variables from inside the function  and put them outside the function so that it will allow the function to be reusable and not fixed

--- Extract_Script_CSV.py
+++ Extract_Script_CSV.py
@@ -1,42 +1,35 @@
-## New Extract Covid Data File ##
-
+## Import Librairies 
 import pandas as pd
 import requests
 from io import StringIO
 import psycopg2
 import os
 
-
 ## Read the csv file 
 url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
 df = pd.read_csv(StringIO(requests.get(url).text))
 
+## Minor transform the csv file ##
 
-## transform the csv file ##
-
-
-
-##Get number of rows
+## Limit the number of rows returned
 trimdata = df.head(50)
 print(trimdata)
-
-
 
 ## store columns in var Test 
 date = trimdata["date"]
 print(date)
 
+##### Connect to Neon or any other database ######
 
-  ##### Connect to Neon or any other database ######
+## Declare Connection variables 
+username = 'neondb_owner'
+password = 'npg_0aLTWcGqE1AJ'
+host = 'ep-lively-term-abff86z3-pooler.eu-west-2.aws.neon.tech'
+port = '5432'
+database = 'neondb'
+sslmode = 'require'  # Neon requires SSL
 
-def ConnectionToDatabase():
-## Declare variables 
-    username = 'neondb_owner'
-    password = 'npg_0aLTWcGqE1AJ'
-    host = 'ep-lively-term-abff86z3-pooler.eu-west-2.aws.neon.tech'
-    port = '5432'
-    database = 'neondb'
-    sslmode = 'require'  # Neon requires SSL
+def ConnectionToDatabase(username,password,host,port,database,sslmode):
 
 ## Test connection  
     try:
@@ -53,6 +46,8 @@ def ConnectionToDatabase():
     except Exception as e:
         print(" Connection failed:", e)
 
+## run the connection function 
+ConnectionToDatabase(username,password,host,port,database,sslmode)
+
 ## Save to database 
 
-ConnectionToDatabase()
