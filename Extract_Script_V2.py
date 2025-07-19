aae9fec268c4dd4ad967a007a3c0d00eddd13547
Michael <59081538+Delta483711@users.noreply.github.com>
2025-07-19 08:04:26 +0100

    Update the python code

--- Extract_Script_V2.py
+++ Extract_Script_V2.py
@@ -4,8 +4,29 @@ import pandas as pd
 import requests
 from io import StringIO
 
+
+## Read the csv file 
 url = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
 df = pd.read_csv(StringIO(requests.get(url).text))
 
+
+## transform the csv file 
+
+
+
+##Get number of rows
+rownum = df.head(1)
+
+
+## store columns in vars 
+date = df["date"]
+
+print(rownum)
+
+
+
 # Save locally
-df.to_csv("data/raw/owid_covid_data.csv", index=False)
+#df.to_csv("data/raw/owid_covid_data.csv", index=False)
+
+
+## Save to database 
