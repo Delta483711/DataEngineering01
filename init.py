889fd27fc4672f4e386f8055818ab8ea0d49884b
Michael <59081538+Delta483711@users.noreply.github.com>
2025-07-19 10:25:32 +0100

    Enhance extraction file
    Created a new python file to call into main python extraction file make the connection to the database code even more reusable

---
+++ init.py
@@ -0,0 +1,21 @@
+# Import Librairies 
+import psycopg2 as ps
+
+## Connect to data Function 
+
+def ConnectionToDatabase(username,password,host,port,database,sslmode):
+
+## Test connection  
+    try:
+        connection = ps.connect(
+            host=host,
+            database=database,
+            user=username,
+            password=password,
+            port=port,
+            sslmode=sslmode
+        )
+        print("Successfully connected to Neon PostgreSQL!")
+        connection.close()
+    except Exception as e:
+        print(" Connection failed:", e)
